0fa78d2d0cac51526339a4816f6268b322f15ed7
microsoftnorman <108091421+microsoftnorman@users.noreply.github.com>
2024-07-22 08:35:29 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+curl -fsSL https://ollama.com/install.sh | sh
+
+# Define the service file path
+SERVICE_FILE="/etc/systemd/system/ollama.service"
+
+# Check if the service file exists
+if [ ! -f "$SERVICE_FILE" ]; then
+  echo "Error: $SERVICE_FILE does not exist."
+  exit 1
+fi
+
+# Check if the environment variable is already set
+if grep -q "Environment=\"OLLAMA_HOST=0.0.0.0\"" "$SERVICE_FILE"; then
+  echo "OLLAMA_HOST is already set to 0.0.0.0. No changes were made."
+else
+  # Add the environment variable to the service file
+  sed -i '/\[Service\]/a Environment="OLLAMA_HOST=0.0.0.0"' "$SERVICE_FILE"
+
+  # Reload the systemd daemon to apply the changes
+  systemctl daemon-reload
+
+  # Restart the Ollama service
+  systemctl restart ollama
+
+  echo "Ollama service updated successfully."
+fi
+
+
+loadmodel() {
+ echo "Running model: $1"
+ ollama pull $1
+}
+
+      loadmodel  "phi3" &
+      loadmodel "llama3" &
+      loadmodel "llama3:70b" &
+      loadmodel "codellama" &
+      loadmodel "mixtral" &
+
+# Wait for 
